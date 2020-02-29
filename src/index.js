b53928a58ba466af705d3aeef2d2d8e3d53c2732
Romain Lancia <romainlancia@gmail.com>
2020-03-01 02:07:15 +1100

    setup build extension for chrome browser

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './Components/App'
+
+const start = document.getElementById('root')
+
+start ? ReactDOM.render(<App />, start) : false
